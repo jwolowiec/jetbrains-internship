6a5b96a94a37542423b95ec63caca6dd9291fc0a
Jakub Wołowiec <kuba.wolowiec03@wp.pl>
2026-03-09 09:34:57 +0100

    Remote unnecessary code and leave starting template

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        {title: "New React Router App"},
+        {name: "description", content: "Welcome to React Router!"},
+    ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <div id="react-app" className="overview-page">
+        <h1>Ready to migrate</h1>
+    </div>;
 }
